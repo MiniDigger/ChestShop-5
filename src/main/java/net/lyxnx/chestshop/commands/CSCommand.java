c815b55a7efede98afe91b5dc948c65cdc9719b4
Lyxnx <a.julnes@gmail.com>
2016-04-15 16:40:43 +0100

    Added command framework

---
+++ src/main/java/net/lyxnx/chestshop/commands/CSCommand.java
@@ -0,0 +1,39 @@
+package net.lyxnx.chestshop.commands;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
+
+/**
+ * Class to make commands easier and so that
+ * they do not have to be registered in the plugin.yml
+ */
+public abstract class CSCommand extends BukkitCommand {
+
+    public CSCommand(final String name, final String permission, final String description, final String usage, final String... aliases) {
+        super(name);
+        this.setDescription(description);
+        this.setAliases(Arrays.asList(aliases));
+        this.setPermission(permission);
+        this.setUsage(usage);
+    }
+
+    @Override
+    public boolean execute(final CommandSender sender, final String alias, final String[] args) {
+        if(!sender.hasPermission(this.getPermission())) {
+            sender.sendMessage("" /* Send permission message from I18n stuff */);
+            return true;
+        }
+
+        run(sender, args);
+        return true;
+    }
+
+    public abstract void run(final CommandSender sender, final String[] args);
+
+    public boolean isPlayer(final CommandSender sender) {
+        return (sender instanceof Player);
+    }
+}
