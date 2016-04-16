1f240e5e4cdfe95540fc872603fd51ceeb1c7487
Lyxnx <a.julnes@gmail.com>
2016-04-16 20:50:29 +0100

    (Should have) fixed the item encoding issue.

--- src/main/java/net/lyxnx/chestshop/items/ItemUtils.java
+++ src/main/java/net/lyxnx/chestshop/items/ItemUtils.java
@@ -1,5 +1,9 @@
-package net.lyxnx.chestshop;
+package net.lyxnx.chestshop.items;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import net.lyxnx.chestshop.ChestShop;
 import net.lyxnx.chestshop.encoding.Base62;
 import net.lyxnx.chestshop.encoding.Base64;
 import net.lyxnx.chestshop.storage.Storage;
@@ -26,6 +30,12 @@ public class ItemUtils {
         this.yaml = new Yaml(new YamlBukkit(), new YamlRepresenter(), new DumperOptions());
     }
 
+    /**
+     * Returns an item's encoded code.
+     * <p />
+     * If the item does not exist, it will be added to the table.
+     * @return The item code of the item.
+     */
     public String getItemCode() {
         try {
             ItemStack clone = new ItemStack(item);
@@ -40,39 +50,24 @@ public class ItemUtils {
                 return Base62.encode(i.getId());
             }
 
-            i = new Item(code);
+            insertItem(code);
 
-            addItem(i);
+            i = getItem(code);
 
             return Base62.encode(i.getId());
-
         } catch(final Exception ex) {
             ex.printStackTrace();
         }
-
         return null;
     }
 
-    public boolean isPresent(final String code) {
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        try(Connection conn = ChestShop.getInstance().getStorage().getConnection()) {
-            ps = conn.prepareStatement("SELECT id FROM items WHERE code=?;");
-            ps.setString(1, code);
-            rs = ps.executeQuery();
-            if(rs.next()) {
-                return true;
-            }
-            rs.close();
-            ps.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            Storage.close(rs, ps);
-        }
-        return false;
-    }
-
+    /**
+     * Returns an Item based off of its item code.
+     * <p />
+     * This should be ran aysnc.
+     * @param code Code of item.
+     * @return An Item equivalent to the input item code or null.
+     */
     public Item getItem(final String code) {
         Item item = null;
         PreparedStatement ps = null;
@@ -82,9 +77,7 @@ public class ItemUtils {
             ps.setString(1, code);
             rs = ps.executeQuery();
             if(rs.next()) {
-                item = new Item();
-                item.setId(rs.getInt("id"));
-                item.setCode(code);
+                item = new Item(rs.getInt("id"), code);
             }
             rs.close();
             ps.close();
@@ -96,12 +89,12 @@ public class ItemUtils {
         return item;
     }
 
-    public void addItem(final Item item) {
+    public void insertItem(final String code) {
         Bukkit.getScheduler().runTaskAsynchronously(ChestShop.getInstance(), () -> {
             PreparedStatement ps = null;
-            try (Connection conn = ChestShop.getInstance().getStorage().getConnection()) {
+            try(Connection conn = ChestShop.getInstance().getStorage().getConnection()) {
                 ps = conn.prepareStatement("INSERT INTO items(code) VALUES(?);");
-                ps.setString(1, item.getCode());
+                ps.setString(1, code);
                 ps.executeUpdate();
                 ps.close();
             } catch (SQLException e) {
@@ -112,8 +105,15 @@ public class ItemUtils {
         });
     }
 
+    @Data
+    @AllArgsConstructor(access = AccessLevel.PACKAGE)
+    public class Item {
+        private final int id;
+        private final String code;
+    }
+
     private class YamlBukkit extends YamlConstructor {
-        public YamlBukkit() {
+        YamlBukkit() {
             this.yamlConstructors.put(new Tag(Tag.PREFIX + "org.bukkit.inventory.ItemStack"), yamlConstructors.get(Tag.MAP));
         }
     }
